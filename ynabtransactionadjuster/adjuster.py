0fa2b08614d142c2a48e7034576b0069e39533bc
dnbasta <daniel.basta@here.com>
2024-03-23 10:47:49 +0100

    made type checking strict

--- ynabtransactionadjuster/adjuster.py
+++ ynabtransactionadjuster/adjuster.py
@@ -40,7 +40,7 @@ class Adjuster:
 
 	@staticmethod
 	def validate_attributes(modifier: TransactionModifier):
-		TransactionModifier.model_validate(modifier.__dict__)
+		TransactionModifier.model_validate(modifier.__dict__, strict=True)
 
 	@staticmethod
 	def validate_instance(modifier: Optional[TransactionModifier]):
