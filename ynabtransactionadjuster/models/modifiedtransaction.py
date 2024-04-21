b1fd06256a5ef2c926c4a4db2e5489f7882b0a74
dnbasta <daniel.basta@here.com>
2024-04-21 09:30:32 +0200

    updated tests

--- ynabtransactionadjuster/models/modifiedtransaction.py
+++ ynabtransactionadjuster/models/modifiedtransaction.py
@@ -7,8 +7,8 @@ from ynabtransactionadjuster.models import Modifier
 
 
 class ModifiedTransaction(BaseModel):
-	original_transaction: Transaction
-	transaction_modifier: Modifier
+	transaction: Transaction
+	modifier: Modifier
 
 	def is_changed(self) -> bool:
 		"""Helper function to determine if transaction has been altered as compared to original one
@@ -21,20 +21,20 @@ class ModifiedTransaction(BaseModel):
 
 	def as_dict(self) -> dict:
 		"""Returns a dictionary representation of the transaction which is used for the update call to YNAB"""
-		t_dict = dict(id=self.original_transaction.id,
-					payee_name=self.transaction_modifier.payee.name,
-					payee_id=self.transaction_modifier.payee.id,
-					date=datetime.strftime(self.transaction_modifier.transaction_date, '%Y-%m-%d'),
-					approved=self.transaction_modifier.approved,
-					cleared=self.transaction_modifier.cleared)
-		if len(self.transaction_modifier.subtransactions) > 0:
-			t_dict['subtransactions'] = [s.as_dict() for s in self.transaction_modifier.subtransactions]
-		if self.transaction_modifier.category:
-			t_dict['category_id'] = self.transaction_modifier.category.id
-		if self.transaction_modifier.flag_color:
-			t_dict['flag_color'] = self.transaction_modifier.flag_color
-		if self.transaction_modifier.memo:
-			t_dict['memo'] = self.transaction_modifier.memo
+		t_dict = dict(id=self.transaction.id,
+					  payee_name=self.modifier.payee.name,
+					  payee_id=self.modifier.payee.id,
+					  date=datetime.strftime(self.modifier.transaction_date, '%Y-%m-%d'),
+					  approved=self.modifier.approved,
+					  cleared=self.modifier.cleared)
+		if len(self.modifier.subtransactions) > 0:
+			t_dict['subtransactions'] = [s.as_dict() for s in self.modifier.subtransactions]
+		if self.modifier.category:
+			t_dict['category_id'] = self.modifier.category.id
+		if self.modifier.flag_color:
+			t_dict['flag_color'] = self.modifier.flag_color
+		if self.modifier.memo:
+			t_dict['memo'] = self.modifier.memo
 
 		return t_dict
 
@@ -46,30 +46,30 @@ class ModifiedTransaction(BaseModel):
 			if self._attribute_changed(a):
 				changed_attributes[a] = self._create_changed_dict(a)
 
-		if (self.transaction_modifier.transaction_date.isocalendar() !=
-				self.original_transaction.transaction_date.isocalendar()):
+		if (self.modifier.transaction_date.isocalendar() !=
+				self.transaction.transaction_date.isocalendar()):
 			changed_attributes['transaction_date'] = self._create_changed_dict('transaction_date')
 
-		if len(self.transaction_modifier.subtransactions) > 0:
+		if len(self.modifier.subtransactions) > 0:
 			changed_attributes['subtransactions'] = self._create_changed_dict('subtransactions')
 
 		return changed_attributes
 
 	def _attribute_changed(self, attribute: str) -> bool:
-		o = self.original_transaction.__getattribute__(attribute)
-		m = self.transaction_modifier.__getattribute__(attribute)
+		o = self.transaction.__getattribute__(attribute)
+		m = self.modifier.__getattribute__(attribute)
 		if o != m:
 			return True
 
 	def _create_changed_dict(self, attribute: str) -> dict:
-		return dict(original=self.original_transaction.__getattribute__(attribute),
-											  changed=self.transaction_modifier.__getattribute__(attribute))
+		return dict(original=self.transaction.__getattribute__(attribute),
+					changed=self.modifier.__getattribute__(attribute))
 
 	@model_validator(mode='after')
 	def check_values(self):
-		if len(self.transaction_modifier.subtransactions) > 1:
-			if len(self.original_transaction.subtransactions) > 1:
+		if len(self.modifier.subtransactions) > 1:
+			if len(self.transaction.subtransactions) > 1:
 				raise ValueError(f"Existing Subtransactions can not be updated")
-			if sum(a.amount for a in self.transaction_modifier.subtransactions) != self.original_transaction.amount:
+			if sum(a.amount for a in self.modifier.subtransactions) != self.transaction.amount:
 				raise ValueError('Amount of subtransactions needs to be equal to amount of original transaction')
 		return self
