f8b2b31875f24d7a501138061bc5d77f04a4b247
dnbasta <daniel.basta@here.com>
2024-04-30 08:23:09 +0200

    added transfer_transaction_id to subtransaction

--- ynabtransactionadjuster/models/subtransaction.py
+++ ynabtransactionadjuster/models/subtransaction.py
@@ -13,8 +13,10 @@ class SubTransaction:
 	:ivar category: The category of the subtransaction
 	:ivar amount: The amount of the subtransaction in milliunits
 	:ivar memo: The memo of the subtransaction
+	:ivar transfer_transaction_id: The transaction id of the correlated transaction in the transfer account
 	"""
 	payee: Payee
 	category: Optional[Category]
 	memo: Optional[str]
 	amount: int
+	transfer_transaction_id: Optional[str]
